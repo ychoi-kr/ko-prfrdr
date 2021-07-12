bfcbfc79a92e46f1d019412b5e9d9581460b4011
Yong Choi <sk8er.choi@gmail.com>
2021-07-12 15:35:40 +0900

    Update youtube_playlist_csv.py

--- youtube_playlist_csv.py
+++ youtube_playlist_csv.py
@@ -4,24 +4,18 @@ import argparse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
-def main(playlist_id):
-    chrome_options = Options()
-    #chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
-
-    playlist_url = "https://www.youtube.com/playlist?list=" + playlist_id
-    driver.get(playlist_url)
-    links = driver.find_elements_by_css_selector("#video-title")
-    
-    for link in links:
-        title = link.text
-        video_url = link.get_attribute('href').split('&')[0]
-        print(f"\"{title}\",\"{video_url}\"")
+from youtube import youtube
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("playlist_id")
     args = parser.parse_args()
-    main(args.playlist_id)
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+
+    for row in youtube.get_playlist_table(driver, args.playlist_id):
+        dquo = "\""
+        print(','.join(map(lambda x: dquo + x + dquo, row)))
