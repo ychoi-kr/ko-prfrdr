994cef894f6b3848238c695e1e35910a9c81b22c
Yong Choi <sk8er.choi@gmail.com>
2021-07-14 14:09:07 +0900

    Update txt_merge.py
    Improve file sorting

--- txt_merge.py
+++ txt_merge.py
@@ -5,7 +5,7 @@ from glob import glob
 import os
 
 
-def main(book_title, directory, encoding, sub_dir='merged'):
+def main(bookname, directory, encoding, sub_dir='merged'):
     if directory:
         os.chdir(directory)
     
@@ -14,7 +14,7 @@ def main(book_title, directory, encoding, sub_dir='merged'):
     
     try:
         with open(f"{sub_dir}/{bookname}.txt", "wt", encoding=encoding) as outfile:
-            for title in glob(f"{book_title}*.txt"):
+            for title in get_sorted_filenames(bookname):
                 infile = open(title, "rt", encoding=encoding)
                 outfile.write(title + '\n')
                 outfile.write('-' * len(title) + '\n')
@@ -22,6 +22,20 @@ def main(book_title, directory, encoding, sub_dir='merged'):
     except UnicodeDecodeError as e:
         print("An Exception raised:", e)
         print("Try again with option: -e utf-8")
+
+
+def get_sorted_filenames(title):
+    possible_delimiter = '-_'
+    for d in possible_delimiter:
+        filenames = glob(f"{title}{d}*.txt")
+        prefix = title + d
+        suffix = '.txt'
+        if all([f.startswith(prefix) and f.endswith(suffix) for f in filenames]):
+            middle = [int(f.replace(prefix, '').replace(suffix, '')) for f in filenames]
+            filenames = [prefix + str(m) + suffix for m in sorted(middle)]
+            break
+
+    return filenames
 
 
 if __name__ == '__main__':
