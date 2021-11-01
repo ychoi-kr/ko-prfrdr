35e7de06e5e9b0e4528ae651793e920e16d44ce7
Yong Choi <sk8er.choi@gmail.com>
2021-11-01 19:40:14 +0900

    Update haeyo.py

--- haeyo.py
+++ haeyo.py
@@ -1,19 +1,3 @@
-# tt = list()
-
-# with open('ko_speech_level.txt') as f:
-    # for line in f.readlines()[1:]:
-        # hasipsio, haeyo, haera, hae = tuple(map(lambda x: x + '.', line.split(',')))
-        # for x in [hasipsio, haera, hae]:
-            # if x != '.' and haeyo != '.':
-                # tt.append((x, haeyo))
-
-# stc = input()
-
-# for src, dst in tt:
-    # stc = stc.replace(src, dst)
-
-# print()
-# print(stc)
 import convert_speech_level
 
 sentence = input()
