757ff73b152ce409125ade1ece3a54376476ab1d
Yong Choi <sk8er.choi@gmail.com>
2021-11-01 19:40:12 +0900

    Update haera.py

--- haera.py
+++ haera.py
@@ -1,20 +1,3 @@
-# tt = list()
-
-# with open('ko_speech_level.txt') as f:
-    # for line in f.readlines()[1:]:
-        # hasipsio, haeyo, haera, hae = tuple(map(lambda x: x + '.', line.split(',')))
-        # for x in [hasipsio, haeyo, hae]:
-            # if x != '.' and haera != '.':
-                # tt.append((x, haera))
-
-# stc = input()
-
-# for src, dst in tt:
-    # stc = stc.replace(src, dst)
-
-# print()
-# print(stc)
-
 import convert_speech_level
 
 sentence = input()
