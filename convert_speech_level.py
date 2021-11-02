89443e9d4c42406d6125190f707500f16800e187
Yong Choi <sk8er.choi@gmail.com>
2021-11-02 10:17:02 +0900

    Update convert_speech_level.py

--- convert_speech_level.py
+++ convert_speech_level.py
@@ -1,28 +1,35 @@
+import os
+
 def convert(sentence, level):
+    f = open(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)),
+        'ko_speech_level.txt')
+        )
     tt = list()
 
-    with open('ko_speech_level.txt') as f:
-        for line in f.readlines()[1:]:
-            col = line.rstrip().split(',')  # hasipsio, haeyo, haera, hae
-            if level == 'hasipsio':
-                src_idx_list = [1, 2, 3]
-                dst_idx = 0
-            elif level == 'haeyo':
-                src_idx_list = [0, 2, 3]
-                dst_idx = 1
-            elif level == 'haera':
-                src_idx_list = [0, 1, 3]
-                dst_idx = 2
-            elif level == 'hae':
-                src_idx_list = [0, 1, 2]
-                dst_idx = 3
-            
-            for src_idx in src_idx_list:
-                if col[src_idx] != '' and col[dst_idx] != '':
-                    tt.append((col[src_idx], col[dst_idx]))
+    for line in f.readlines()[1:]:
+        col = line.rstrip().split(',')  # hasipsio, haeyo, haera, hae
+        if level == 'hasipsio':
+            src_idx_list = [1, 2, 3]
+            dst_idx = 0
+        elif level == 'haeyo':
+            src_idx_list = [0, 2, 3]
+            dst_idx = 1
+        elif level == 'haera':
+            src_idx_list = [0, 1, 3]
+            dst_idx = 2
+        elif level == 'hae':
+            src_idx_list = [0, 1, 2]
+            dst_idx = 3
+        
+        for src_idx in src_idx_list:
+            if col[src_idx] != '' and col[dst_idx] != '':
+                tt.append((col[src_idx], col[dst_idx]))
    
     for src, dst in tt:
         sentence = sentence.replace(src + '.', dst + '.')
+        sentence = sentence.replace(src + '!', dst + '!')
+        sentence = sentence.replace(src + '?', dst + '?')
 
     return sentence
 
