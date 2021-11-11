666dc3fbc3c4c0e412923f7d6d6df480ebb88a07
Yong Choi <sk8er.choi@gmail.com>
2021-11-12 04:51:13 +0900

    Update stylechk.py

--- stylechk.py
+++ stylechk.py
@@ -5,7 +5,8 @@ import chk_manuscript
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--filename", help="filename", type=str)
 parser.add_argument("-r", "--rulefile", nargs='+', 
-                    default=['en_ko_style_correction.json', 'jp_ko_style_correction.json', 'wikibook_style_guide.json'])
+                    default=['en_ko_style_correction.json', 'jp_ko_style_correction.json',
+                             'wikibook_style_guide.json', 'simple_style.json'])
 args = parser.parse_args()
 
 chk_manuscript.main(args.filename, args.rulefile)
