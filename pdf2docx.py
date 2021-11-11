eb4a3ffd48c9b4be74dad68f401c602c476a0f3e
Yong Choi <sk8er.choi@gmail.com>
2021-11-12 04:53:05 +0900

    Update pdf2docx.py

--- pdf2docx.py
+++ pdf2docx.py
@@ -3,10 +3,11 @@ import os
 from pdf2docx import Converter
 
 
-pdf_file = max(glob.glob('*.pdf'), key=os.path.getctime)
-docx_file = pdf_file + '.docx'
+if __name__ == '__main__':
+    pdf_file = max(glob.glob('*.pdf'), key=os.path.getctime)
+    docx_file = pdf_file + '.docx'
 
-# convert pdf to docx
-cv = Converter(pdf_file)
-cv.convert(docx_file)      # all pages by default
-cv.close()
+    cv = Converter(pdf_file)
+    cv.convert(docx_file)
+    #cv.convert(docx_file, multi_processing=True, cpu_count=4)
+    cv.close()
