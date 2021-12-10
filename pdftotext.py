ed6b96a3faaaf7f396b4abc9c1e2debf25aa4990
sk8erchoi <sk8er.choi@gmail.com>
2021-12-10 09:35:32 +0900

    feat: password option

--- pdftotext.py
+++ pdftotext.py
@@ -6,14 +6,16 @@ import re
 import argparse
 from pathlib import Path
 
-from pdfrw import PdfReader
+from pikepdf import Pdf
 
 
-def main(pdf_file, header, footer):
+def main(pdf_file, header, footer, password):
     y = header
-    H = height(pdf_file) - header - footer
     W = 700
-    cmd = f"pdftotext -y {y} -H {H} -W {W} -nopgbrk {pdf_file} -"
+    height = page_height(pdf_file, password)
+    options_to_remove_header_and_footer = f'-y {header} -H {height - header - footer} -W {W}' if height else ''
+    user_password_option = f'-upw {password}' if password else ''
+    cmd = f"pdftotext {options_to_remove_header_and_footer} {user_password_option} -nopgbrk {pdf_file} -"
     s = os.popen(cmd).read()
     with open(Path(pdf_file).stem + ".txt", 'wt') as f:
         f.write(
@@ -21,8 +23,17 @@ def main(pdf_file, header, footer):
         )
 
 
-def height(pdf_file):
-    return round(float(PdfReader(pdf_file).pages[0].MediaBox[3]))
+def page_height(filename, password):
+    try:
+        if password:
+            pdf = Pdf.open(filename, password=password)
+        else:
+            pdf = Pdf.open(filename)
+        firstpage = pdf.pages[0]
+        return int(firstpage.MediaBox[3])
+    except:
+        print('Cannot get page height. Fallback.')
+        return None
 
 
 def nnp():
@@ -40,6 +51,7 @@ if __name__ == '__main__':
     parser.add_argument("pdf_file", type=str)
     parser.add_argument("--header", default=70, type=int) 
     parser.add_argument("--footer", default=50, type=int) 
+    parser.add_argument("--password", type=str) 
     args = parser.parse_args()
-    main(args.pdf_file, args.header, args.footer)
+    main(args.pdf_file, args.header, args.footer, args.password)
 
