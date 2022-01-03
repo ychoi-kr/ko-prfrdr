248d94abd0595b9e6f9616d72f9118557c1c01f5
sk8erchoi <sk8er.choi@gmail.com>
2022-01-04 00:42:13 +0900

    feat: add concat()

--- kostr.py
+++ kostr.py
@@ -16,3 +16,33 @@ def join(seq):
 
     return reduce(join_two, seq)
 
+
+def concat(*seq):
+    def concat_two(a, b):
+        if re.match('[ㄱ-ㅎ]', a[-1]) and re.match('[ㅏ-ㅣ]', b):
+            #return chr(ord('가') + (ord(b) - ord('ㅏ')) * (len(kosound.FINAL_CONSONANTS) + 1))
+            return ''.join([
+                    a[:-1],
+                    chr(
+                        ord('가')
+                        + (kosound.FIRST_CONSONANTS.find(a[-1]) * len(kosound.VOWELS) * (len(kosound.FINAL_CONSONANTS)+1))
+                        + ((len(kosound.FINAL_CONSONANTS) + 1) * (ord(b) - ord('ㅏ')))
+                    )
+                    ])
+        elif re.match('[가-히]', a[-1]) and not kosound.hasfinalconsonant(a[-1]) and b in kosound.CONSONANTS:
+            return ''.join([a[:-1], chr(ord(a[-1]) + kosound.FINAL_CONSONANTS.find(b) + 1)])
+        else:
+            return ''.join([a, b])
+
+    return reduce(concat_two, seq)
+
+
+if __name__ == '__main__':
+    print("concat('ㄱ', 'ㅏ')", concat('ㄱ', 'ㅏ'))
+    print("concat('가', 'ㄴ')", concat('가', 'ㄴ'))
+    print("concat('가바', 'ㄴ')", concat('가바', 'ㄴ'))
+    print("concat('ㄱ', 'ㅏ', 'ㄱ')", concat('ㄱ', 'ㅏ', 'ㄱ'))
+    print("concat('ㄱ', 'ㅏ', 'ㄴ')", concat('ㄱ', 'ㅏ', 'ㄴ'))
+    print("concat('ㄱ', 'ㅏ', 'ㄷ')", concat('ㄱ', 'ㅏ', 'ㄷ'))
+    print("concat('ㄱ', 'ㅏ', 'ㄹ')", concat('ㄱ', 'ㅏ', 'ㄹ'))
+    print("concat('나ㅃ', 'ㅡ', '다')", concat('나ㅃ', 'ㅡ', '다'))
