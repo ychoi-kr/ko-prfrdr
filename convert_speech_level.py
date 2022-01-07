7ea2f50fa95955b16da13bc26391ee1e6e49d093
Yong Choi <sk8er.choi@gmail.com>
2022-01-07 17:27:53 +0900

    refactor: extract load()

--- convert_speech_level.py
+++ convert_speech_level.py
@@ -1,13 +1,41 @@
 import os
 
-def convert(sentence, level):
-    f = open(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)),
-        'ko_speech_level.txt')
-        )
+def load(fp, level):
+
     tt = list()
 
-    for line in f.readlines()[1:]:
+    for line in fp.readlines()[1:]:
+        col = line.rstrip().split(',')  # hasipsio, haeyo, haera, hae
+        if level == 'hasipsio':
+            src_idx_list = [1, 2, 3]
+            dst_idx = 0
+        elif level == 'haeyo':
+            src_idx_list = [0, 2, 3]
+            dst_idx = 1
+        elif level == 'haera':
+            src_idx_list = [0, 1, 3]
+            dst_idx = 2
+        elif level == 'hae':
+            src_idx_list = [0, 1, 2]
+            dst_idx = 3
+        
+        for src_idx in src_idx_list:
+            if col[src_idx] != '' and col[dst_idx] != '':
+                tt.append((col[src_idx], col[dst_idx]))
+    return tt
+
+def convert(sentence, level):
+    fp = open(
+            os.path.join(os.path.dirname(os.path.realpath(__file__)),
+            'ko_speech_level.txt'
+        ),
+        "rt",
+        encoding="UTF8"
+    )
+    
+    tt = load(fp, level)
+
+    for line in fp.readlines()[1:]:
         col = line.rstrip().split(',')  # hasipsio, haeyo, haera, hae
         if level == 'hasipsio':
             src_idx_list = [1, 2, 3]
