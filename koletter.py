44f915183f2f5057c34b2861b984d92a63a5e7b8
sk8erchoi <sk8er.choi@gmail.com>
2022-01-09 18:09:08 +0900

    feat: final nieun

--- koletter.py
+++ koletter.py
@@ -6,7 +6,7 @@ import kosound as ks
 # Unicode decimal of the first korean letter ('가')
 _FIRST_ = 44032
 
-# ALL letters
+# ALL letters ('가'-'힣')
 KL_ALL = "".join(
     [
         chr(_FIRST_ + (len(ks.VOWELS) * (len(ks.FINAL_CONSONANTS) + 1) * i) + ((len(ks.FINAL_CONSONANTS) + 1) * j) +  k)
@@ -28,16 +28,18 @@ KL_NFC = ''.join(
 # letters with final consonants
 KL_WFC = ''.join(sorted(set(KL_ALL) - set(KL_NFC)))
 
-# letters with final consonants which is singular
+# letters with final consonants except '들'(letter that represents plural)
 KL_WFS = ''.join(sorted(set(KL_WFC) - set(['들'])))
 
 # letters with final lieul
 KL_WFL = ''.join([chr(ord(c) + 8) for c in KL_NFC])
 
-# letters with final consonants but lieul
-KL_WFB = ''.join(sorted(set(KL_NFC) - set(KL_WFL)))
+# letters with final nieun
+KL_WFN = ''.join([chr(ord(c) + 4) for c in KL_NFC])
 
-# letters without final consonants or with final lieul
-KL_NFL = ''.join(sorted(KL_NFC + KL_WFL))
+# letters without final lieul
+KL_NFL = ''.join(sorted(set(KL_ALL) - set(KL_WFL)))
 
+# letters without final nieun
+KL_NFN = ''.join(sorted(set(KL_ALL) - set(KL_WFN)))
 
