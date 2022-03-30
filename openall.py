9e0108aac63f18b4eeb8c9b7dac7a4ee6b1fb18f
Yong Choi <sk8er.choi@gmail.com>
2022-03-30 10:22:14 +0900

    Update openall.py

--- openall.py
+++ openall.py
@@ -1,11 +1,19 @@
 import glob
 import win32com.client as win32
 import os
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-R", "--recursive", action="store_true")
+args = parser.parse_args()
 
 word = win32.Dispatch('Word.Application')
 word.Visible = True
 
-for f in glob.iglob(r'**\*.docx', recursive=True):
+files = glob.iglob(r'**/*.docx', recursive=True) if args.recursive else glob.iglob(r'*.docx')
+
+for f in files:
     p = os.getcwd() + os.sep + f
     print(f'Opening {p}...')
     word.Documents.Open(p)
