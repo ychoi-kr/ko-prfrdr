020cd2dbb8734c002c9652d23a60f14fa9e7b9bb
sk8erchoi <sk8er.choi@gmail.com>
2022-04-29 08:35:21 +0900

    style: remove space

--- spellchk.py
+++ spellchk.py
@@ -9,7 +9,7 @@ parser.add_argument("filename", nargs="?", type=str)
 
 default_rules = ['ko_spelling_rules.json', 'ko_spacing_rules.json',
          'foreign_sound_rules.json']
-parser.add_argument("-r", "--rulefile", default=' ' .join(default_rules))
+parser.add_argument("-r", "--rulefile", default=' '.join(default_rules))
 parser.add_argument("--debug", action="store_true")
 args = parser.parse_args()
     
