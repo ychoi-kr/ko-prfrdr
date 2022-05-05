2dff0a48442d01a8ddc49a484a4c2901de5c2456
sk8erchoi <sk8er.choi@gmail.com>
2022-05-05 21:28:49 +0900

    feat: add 'content' command

--- wikidocs.py
+++ wikidocs.py
@@ -6,13 +6,22 @@ import sys
 
 cmd = sys.argv[1]
 
-url = 'https://wikidocs.net/book/' + sys.argv[2]
-with urllib.request.urlopen(url) as f:
-    html = f.read().decode('utf-8')
+if cmd == "toc":
+    url = 'https://wikidocs.net/book/' + sys.argv[2]
+    with urllib.request.urlopen(url) as f:
+        html = f.read().decode('utf-8')
+    
+    soup = BeautifulSoup(html, 'html.parser')
+    titles = soup.select('.list-group-item > span')
+    for title in titles[1:]:
+         s = title.select('span')[0].text.strip()
+         print(s)
 
-soup = BeautifulSoup(html, 'html.parser')
-titles = soup.select('.list-group-item > span')
-for title in titles[1:]:
-     s = title.select('span')[0].text.strip()
-     print(s)
-
+elif cmd == "content":
+    url = 'https://wikidocs.net/' + sys.argv[2]
+    with urllib.request.urlopen(url) as f:
+        html = f.read().decode('utf-8')
+    
+    soup = BeautifulSoup(html, 'html.parser')
+    for content in soup.select('div.page-content'):
+        print(content.text)
