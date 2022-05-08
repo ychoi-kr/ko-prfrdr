5336e93d62d3c96b90632dd3043865b2b5371e23
sk8erchoi <sk8er.choi@gmail.com>
2022-05-08 23:42:57 +0900

    feat: remove duplicate and sort result of joinseq

--- utils.py
+++ utils.py
@@ -1,2 +1,5 @@
+from functools import reduce
+
 def joinseq(*seq):
-    return '|'.join([x.strip('|') for x in seq])
+    return '|'.join(sorted(set(reduce(lambda x, y: x + y, [x.split('|') for x in seq]))))
+
