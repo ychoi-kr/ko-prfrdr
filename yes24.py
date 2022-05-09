3c86e409c732d312653323ab1511faf19d0d1f3c
sk8erchoi <sk8er.choi@gmail.com>
2022-05-09 23:10:43 +0900

    feat: get category by name

--- yes24.py
+++ yes24.py
@@ -1,8 +1,46 @@
+#!/usr/bin/python3
+
 from urllib import parse
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import argparse
 
+
+categorymap = {
+    "art": "001001007",
+    "biz": "001001025",
+    "elementary": "001001044",
+    "exam": "001001015",
+    "humanity": "001001019",
+    "it": "001001003",
+    "kid": "001001016",
+    "kids": "001001016",
+    "literature": "001001046",
+    "middle": "001001013",
+    "sci": "001001002",
+    "self": "001001026",
+    "teen": "001001005",
+    "test": "001001015",
+    "univ": "001001014",
+    "경영": "001001025",
+    "경제": "001001025",
+    "과학": "001001002",
+    "대학교재": "001001014",
+    "문학": "001001046",
+    "수험서": "001001015",
+    "어린이": "001001016",
+    "예술": "001001007",
+    "인문": "001001019",
+    "자격증": "001001015",
+    "자기개발": "001001026",
+    "자기계발": "001001026",
+    "자연과학": "001001002",
+    "중고등": "001001013",
+    "중고등참고서": "001001013",
+    "청소년": "001001005",
+    "초등": "001001044",
+    "초등참고서": "001001044",
+}
 
 def main(keyword, order, category):
     site = "http://www.yes24.com"
@@ -15,15 +53,20 @@ def main(keyword, order, category):
                   "평점순": "CONT_NT",
                   "리뷰순": "REVIEW_CNT"}
 
-    qry = parse.urlencode([
+    qrylist = [
         ("domain", "BOOK"),
         ("query", ' '.join(keyword)),
         ("order", basefilter[order]),
-        ("dispno2", category)
-    ])
+    ]
 
-    url = site + "/Product/Search?" + qry
-    
+    if category and category.lower() != "all":
+        if category.startswith('0'):
+            qrylist.append(("dispno2", category))
+        else:
+            qrylist.append(("dispno2", categorymap[category.lower()]))
+
+    qrystr = parse.urlencode(qrylist)
+    url = site + "/Product/Search?" + qrystr
     print("Opening", url, "...\n")
 
     with urlopen(url) as f:
