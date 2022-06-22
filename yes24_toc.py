8b022749401b23d2af7b9ab44fa92a2434282596
sk8erchoi <sk8er.choi@gmail.com>
2022-06-22 15:58:09 +0900

    feat: add strip option

--- yes24_toc.py
+++ yes24_toc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import re
 import argparse
 
 import html2text
@@ -7,13 +8,25 @@ import html2text
 import yes24_bookinfo
 
 
-def main(goodsid):
-    toc = yes24_bookinfo.bookinfo(goodsid, showurl=None)["toc"]
-    print(html2text.html2text(toc))
+def main(goodsid, strip):
+
+    # let users just copy & paste URL
+    goodsid = goodsid.replace("http://www.yes24.com/Product/Goods/", '')
+
+    bookinfo = yes24_bookinfo.bookinfo(goodsid, showurl=None)
+    print("TITLE:" + bookinfo["title"] + '\n')
+
+    print("Table of Contents:")
+    toc = bookinfo["toc"]
+    for line in html2text.html2text(toc).splitlines():
+        if strip:
+            line = line.strip().strip('**').strip('__')
+        print(line)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("goodsid", type=str)
+    parser.add_argument("--strip", action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
-    main(args.goodsid)
+    main(args.goodsid, args.strip)
