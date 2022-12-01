81f41c6f8bf2ef58ce591d62a26cae9c6f4e06a8
sk8erchoi <sk8er.choi@gmail.com>
2022-12-01 15:44:29 +0900

    fix: fix arguments

--- suggest.py
+++ suggest.py
@@ -6,9 +6,11 @@ import proofread as prf
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", nargs="?", type=str)
-parser.add_argument("-r", "--rulefile", default=' '.join(prf.suggest_rules))
+parser.add_argument("--rule", type=str)
+parser.add_argument("--rulefile", default=' '.join([x[0] for x in prf.suggest_rules]))
 parser.add_argument("--show_all_lines", action="store_true")
 parser.add_argument("--debug", action="store_true")
+parser.add_argument("--profile", action="store_true")
 args = parser.parse_args()
     
-prf.main(args.filename, args.rulefile, args.show_all_lines, args.debug)
+prf.main(args.filename, args.rulefile, args.rule, args.show_all_lines, args.debug, args.profile)
