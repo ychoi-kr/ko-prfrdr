3a96c580e53b937a419f7e8e340b12702af1a4af
Yong Choi <ychoi_kr@naver.com>
2023-08-08 20:25:52 +0900

    feat: update prompt

--- proof-gpt.py
+++ proof-gpt.py
@@ -65,7 +65,9 @@ def check(sentence, rule, cache):
     subject, content = get_page(rule, cache)
     prompt = f"#{subject}\n{content}\n"
     prompt += "---\n"
-    prompt += "위의 규칙에 따라 입력 문장을 검사하고, 결과를 다음과 같은 JSON으로 응답해 주세요. 위의 규칙과 관계없는 것은 검사 결과에 포함하지 마세요.\n"
+    prompt += "위의 규칙에 따라 입력 문장을 검사하고, 결과를 다음과 같은 JSON으로 응답해 주세요. "
+    prompt += "이때 bad와 good이 너무 짧으면 위치를 알아보기 힘들므로 적당한 길이로 나타내 주세요.\n "
+    prompt += "위의 규칙과 관계없는 것은 검사 결과에 포함하지 마세요.\n"
     prompt += output_format.replace("<rule URL>", rule_url)
     prompt += "\n---\n입력:"
     prompt += f"\n{sentence}"
